6bcd27cbe7ea479d9e53100c796785fec90c8d1d
vennyssious <ygorch@users.noreply.github.com>
2018-04-02 14:29:57 -0300

    Correção em exportar quando não há conteúdo

--- js/app/models/Export.js
+++ js/app/models/Export.js
@@ -1,7 +1,8 @@
 class Export  {
 
   toCSV (data){
-    console.log(data)
+  	if(data.length < 1)
+  		return 'javascript:void(0);';
     let CSVData = Object.entries(data).reduce((value, entry) =>
       `${value}\r\n${entry[1]._currentdate},${entry[1]._ip},${entry[1]._persona===1?'B2C':'B2B'},${entry[1]._name},${entry[1]._email}`,
       'date,ip,persona,name,email'
