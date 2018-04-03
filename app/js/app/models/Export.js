8b2d31ef5317854741acc15ce07e6ea4d7f1d377
vennyssious <Vini1703>
2018-04-02 23:22:57 -0300

    Exportar nome e e-mail

--- app/js/app/models/Export.js
+++ app/js/app/models/Export.js
@@ -5,11 +5,9 @@ class Export  {
       header : [
         'email,nome,ip,tipo,data_hora',
         'email,persona,pergunta,resposta,pergunta,resposta,pergunta,resposta',
-        'email'
+        'email,name'
       ],
-      body : [
-        '', '', ''
-      ],
+      body : ['', '', ''],
       name : [
         'leads.csv',
         'pesquisa.csv',
@@ -30,7 +28,7 @@ class Export  {
     this.formats.body = [
       `${data._email},${data._name},${data._ip},${parseInt(data._persona)===1?'B2C':'B2B'},${this._dateFormat(data._currentdate)}`,
       `${data._email},${data._ip},${parseInt(data._persona)===1?'CONTRATADO':'CONTRATANTE'},Você já teve problemas em algum processo seletivo? E em relação a análise de competências técnicas na área de Marketing?,${data._questionFirst},Como você lidou ou está lidando com isso?,${data._questionSecond},Qual seria a solução ideal para resolver isso?,${data._questionThird}`,
-      `${data._email}`
+      `${data._email},${data._name}`
     ];
   	return this.formats[dataID][format];
   }
